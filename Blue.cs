00491fc0ce91c6ec5090fd504030b3371294946d
FedorovPetya <fedorovpetya2007@yandex.ru>
2025-09-17 22:37:42 +0300

    Add files via upload

---
+++ Blue.cs
@@ -0,0 +1,137 @@
+﻿namespace Lab3
+{
+    public class Blue
+    {
+        public double Task1(int n, int glass, int norma)
+        {
+            double milk = 0;
+
+            // code here
+            for (int i = 0; i < n; i++)
+            {
+               double weight = double.Parse(Console.ReadLine());
+               if (weight < norma)
+               {
+                    milk += glass;
+               }
+            }
+            milk /= 1000.0; 
+            // end
+
+            return milk;
+        }
+        public (int first, int second, int third, int fourth) Task2(int n)
+        {
+            int first = 0, second = 0, third = 0, fourth = 0;
+
+            // code here
+            for (int i = 0; i < n; i++)
+            {
+                double x = double.Parse(Console.ReadLine());
+                double y = double.Parse(Console.ReadLine());
+                if (x > 0 && y > 0)
+                {
+                    first++;
+                }
+                else if (x < 0 && y > 0)
+                {
+                    second++;
+                }
+                else if (x < 0 && y > 0)
+                {
+                    third++;
+                }
+                else
+                {
+                    fourth++;   
+                }
+            }
+            // end
+
+            return (first, second, third, fourth);
+        }
+        public int Task3(int n)
+        {
+            int count = 0;
+
+            // code here
+            for (int i = 0; i < n; i++)
+            {
+                int firstGrade = int.Parse(Console.ReadLine());
+                int secondGrade = int.Parse(Console.ReadLine());
+                int thirdGrade = int.Parse(Console.ReadLine());
+                int fourthGrade = int.Parse(Console.ReadLine());
+                if (firstGrade > 3 || secondGrade > 3 || thirdGrade > 3 || fourthGrade > 3)
+                {
+                    count++;
+                }
+
+            }
+            // end
+
+            return count;
+        }
+        public (int tasks, int serias) Task4(int time, int tasks)
+        {
+            int serias = 0;
+
+            // code here
+            int seriasTime, taskTime = 10; 
+            while (time < 24.00)
+            {
+                if (tasks > 0)
+                {
+                    time += taskTime;
+                    taskTime += 5;
+                    tasks--;
+                }
+                else
+                {
+                    seriasTime = int.Parse(Console.ReadLine());
+                    time += seriasTime;
+                    serias++;
+                }
+            }
+            // end
+
+            return (tasks, serias);
+        }
+        public (int power, int agility, int intellect) Task5(int power, int agility, int intellect, int number)
+        {
+
+            // code here
+            switch (number)
+            {
+                case 1:
+                    power += 10;
+                    intellect -= 5;
+                    break;
+                case 2:
+                    agility += 5;
+                    intellect -= 5;
+                    power -= 5;
+                    break;
+                case 3:
+                    power += 10;
+                    intellect -= 5;
+                    break;
+                case 4:
+                    agility += 15;
+                    power -= 10;
+                    intellect -= 10;
+                    break;
+                case 5:
+                    intellect += 7;
+                    power -= 5;
+                    break;
+            }
+            if (power < 0) power = 0;
+            if (agility < 0) agility = 0;
+            if (intellect < 0) intellect = 0;
+                    
+            // end
+
+            return (power, agility, intellect);
+        }
+    }
+}
