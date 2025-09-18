589f1e64de9d223d352abd35ce844e78b49a7ff3
Arseniy <arseniytishakov@mail.ru>
2025-09-18 21:36:57 +0300

    Update Purple.cs

--- Lab3/Purple.cs
+++ Lab3/Purple.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http.Headers;
+using System.Globalization;
+using System.Net.Http.Headers;
+using System.Xml.Schema;
 
 namespace Lab3
 {
@@ -9,7 +11,13 @@ namespace Lab3
             int count = 0;
 
             // code here
-
+            for (int i = 0; i < n; i++)
+            {
+                double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                double y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                double Dist = Math.Sqrt(x * x + y * y);
+                if (Dist <= (double)r2 && Dist >= (double)r1) count++;
+            }
             // end
 
             return count;
@@ -20,7 +28,21 @@ namespace Lab3
             double average = 0;
 
             // code here
+            double sum = 0;
+            if (n != 0)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    int a = int.Parse(Console.ReadLine());
+                    int b = int.Parse(Console.ReadLine());
+                    int c = int.Parse(Console.ReadLine());
+                    int d = int.Parse(Console.ReadLine());
+                    sum += (double)(a + b + c + d) / 4.0;
+                    if (a == 2 || b == 2 || c == 2 || d == 2) count++;
+                }
 
+                average = sum / (double)n;
+            }
             // end
 
             return (count, average);
@@ -30,6 +52,21 @@ namespace Lab3
             double avgMark = 0;
 
             // code here
+            int theory, practice, mark;
+            int n = exams;
+            double score;
+            while (exams > 0)
+            {
+                theory = int.Parse(Console.ReadLine());
+                practice = int.Parse(Console.ReadLine());
+                score = 0.4 * theory + 0.6 * practice;
+                if (score > 85) mark = 5;
+                else if (score > 70 && score <= 85) mark = 4;
+                else if (score > 50 && score <= 70) mark = 3;
+                else mark = 2;
+                avgMark += (double)mark / (double)n;
+                exams--;
+            }
 
             // end
 
@@ -39,9 +76,28 @@ namespace Lab3
         {
             string solution = "Код не подобран";
             int attempts = 0;
-
             // code here
-
+            while (attempts < limit)
+            {
+                string currentCode = "";
+                attempts++;
+                for (int i = 0; i < 3; i++)
+                {
+                    string newElement = Console.ReadLine();
+                    if (newElement == "-1")
+                    {
+                        solution = "Аварийный выход!";
+                        return (solution, attempts);
+                    }
+                    currentCode += newElement;
+                }
+                if (int.Parse(currentCode) == code)
+                {
+                    solution = "Доступ разрешен!";
+                    return (solution, attempts);
+                }
+            }
+            solution = "Система заблокирована!";
             // end
 
             return (solution, attempts);
@@ -51,9 +107,30 @@ namespace Lab3
             double luck = 0;
 
             // code here
-
+            for (int i = a; i < a + n; i++)
+            {
+                int currentMonthDay = i % 30;
+                if (currentMonthDay != 0)
+                {
+                    switch (currentMonthDay % 7)
+                    {
+                        case 1:
+                            luck = Math.Min(luck * 1.5, 100);
+                            break;
+                        case 4:
+                            luck = Math.Max(luck - 10, 0);
+                            break;
+                        case 0:
+                            if (luck < 50) luck = 55;
+                            break;
+                        default:
+                            luck = Math.Min(luck + 5, 100);
+                            break;
+                    }
+                }
+                else luck = Math.Min(luck + 5, 100);
+            }
             // end
-
             return luck;
         }
     }
