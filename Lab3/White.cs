c6778565dd5921508097dca238660b022047dd61
Alyssa Semenova <alyssa_semenova@mail.ru>
2025-09-22 12:11:37 +0300

    Update White.cs

--- Lab3/White.cs
+++ Lab3/White.cs
@@ -1,4 +1,4 @@
-﻿using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Lab3
 {
@@ -9,7 +9,12 @@ namespace Lab3
             double averageHeight = 0;
 
             // code here
-
+            for (int i = 0; i < n; i++)
+            {
+                int.TryParse(Console.ReadLine(), out int height);
+                // что-то поделала
+            }
+            Console.WriteLine($"My answer {averageHeight}");
             // end
 
             return averageHeight;
@@ -19,7 +24,12 @@ namespace Lab3
             double bestResult = 0;
 
             // code here
-
+            for (int i = 0; i < n; i++)
+            {
+                double.TryParse(Console.ReadLine(), out double time);
+                Console.WriteLine($"The time is {time}");
+                // что-то поделала
+            }
             // end
 
             return bestResult;
