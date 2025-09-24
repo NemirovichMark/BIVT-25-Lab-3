f7706e5d212bbfdd795570a6739652c16802f04a
FuntusovDmitry <gg0140615@gmail.com>
2025-09-24 21:13:13 +0300

    Add files via upload

--- Lab3/Blue.cs
+++ Lab3/Blue.cs
@@ -7,9 +7,16 @@
             double milk = 0;
 
             // code here
-
-            // end
-
+            for (int i = 0; i < n; i++)
+            {
+                double weight = Convert.ToDouble(Console.ReadLine());
+                if (weight < norma)
+                {
+                    milk += glass;
+                }
+            }
+            milk /= 1000;
+            //end
             return milk;
         }
         public (int first, int second, int third, int fourth) Task2(int n)
@@ -17,7 +24,27 @@
             int first = 0, second = 0, third = 0, fourth = 0;
 
             // code here
-
+            for (int i = 0; i < n; i++)
+            {
+                double x = Convert.ToDouble(Console.ReadLine());
+                double y = Convert.ToDouble(Console.ReadLine());
+                if (x > 0 && y > 0)
+                {
+                    first++;             
+                }
+                else if (x<0 && y > 0)
+                {
+                    second++;
+                }
+                else if (x < 0 && y < 0)
+                {
+                    third++;
+                }
+                else if (x > 0 && y < 0)
+                {
+                    fourth++;
+                }
+            }
             // end
 
             return (first, second, third, fourth);
@@ -27,7 +54,17 @@
             int count = 0;
 
             // code here
-
+            for (int i = 0; i < n; i++)
+            {
+                int ocenka1_ = Convert.ToInt32(Console.ReadLine());
+                int ocenka2_ = Convert.ToInt32(Console.ReadLine());
+                int ocenka3_ = Convert.ToInt32(Console.ReadLine());
+                int ocenka4_ = Convert.ToInt32(Console.ReadLine());
+                if ((ocenka1_ != 2 && ocenka1_ != 3) && (ocenka2_ != 2 && ocenka2_ != 3) && (ocenka3_ != 2 && ocenka3_ != 3) && (ocenka4_ != 2 && ocenka4_ != 3))
+                {
+                    count++;
+                }
+            }
             // end
 
             return count;
@@ -37,7 +74,22 @@
             int serias = 0;
 
             // code here
-
+            int seriasTime, taskTime = 10;
+            do
+            {
+                if (tasks > 0)
+                {
+                    time += taskTime;
+                    taskTime += 5;
+                    tasks--;
+                }
+                else
+                {
+                    seriasTime = Convert.ToInt32(Console.ReadLine());
+                    time += seriasTime;
+                    serias++;
+                }
+            } while (time < 24 * 60);
             // end
 
             return (tasks, serias);
@@ -46,6 +98,51 @@
         {
 
             // code here
+            switch (number)
+            {
+                case 1:
+                    power += 10;
+                    intellect -= 5;
+                    break;
+                case 2:
+                    agility += 5;
+                    power -= 5;
+                    intellect -= 5;
+                    break;
+                case 3:
+                    power += 10;
+                    intellect -= 5;
+                    break;
+                case 4:
+                    agility += 15;
+                    power -= 10;
+                    intellect -= 10;
+                    break;
+                case 5:
+                    intellect += 7;
+                    power -= 5;
+                    break;
+            }
+            if (power < 0) {
+                do
+                {
+                    power++;
+                } while (power !< 0);
+            }
+            if (agility < 0)
+            {
+                do
+                {
+                    agility++;
+                } while (agility !< 0);
+            }
+            if (intellect < 0)
+            {
+                do
+                {
+                    intellect++;
+                } while (intellect !< 0);
+            }
 
             // end
 
