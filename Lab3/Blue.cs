d18a688db8a39e0bf184ef16f5f298af64156796
Liner <133678221+Liner11@users.noreply.github.com>
2025-09-26 10:38:00 +0300

    upd

--- Lab3/Blue.cs
+++ Lab3/Blue.cs
@@ -7,7 +7,17 @@
             double milk = 0;
 
             // code here
-
+            double weight = 0;
+            double glassMl = glass * 0.001;
+            for (int x = 0; x < n; x++)
+            {
+                while (double.TryParse(Console.ReadLine().Replace('.', ','), out weight) != true) ;
+                if (weight < norma)
+                {
+                    milk += glassMl;
+                }
+            }
+            milk = Math.Round(milk, 2);
             // end
 
             return milk;
@@ -17,7 +27,16 @@
             int first = 0, second = 0, third = 0, fourth = 0;
 
             // code here
-
+            for (int i = 0; i < n; i++)
+            {
+                double x, y = 0;
+                while (double.TryParse(Console.ReadLine().Replace('.', ','), out x) != true);
+                while (double.TryParse(Console.ReadLine().Replace('.', ','), out y) != true);
+                if (x > 0 && y > 0) first++;
+                else if (x < 0 && y > 0) second++;
+                else if (x < 0 && y < 0) third++;
+                else if (x > 0 && y < 0) fourth++;
+            }
             // end
 
             return (first, second, third, fourth);
@@ -27,7 +46,17 @@
             int count = 0;
 
             // code here
-
+            for (int i = 0; i < n; i++)
+            {
+                int score = 0;
+                bool deb = true;
+                for (int k = 0; k < 4; k++)
+                {
+                    while (int.TryParse(Console.ReadLine(), out score) != true) ;
+                    if (score == 2 || score == 3) deb = false;
+                }
+                count += Convert.ToInt16(deb);
+            }
             // end
 
             return count;
