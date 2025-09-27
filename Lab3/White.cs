5de93d804ecffa785a23de23034d501be523e4d5
blleee3 <dadadaromka@gmail.com>
2025-09-27 22:11:13 +0300

    Update White.cs

--- Lab3/White.cs
+++ Lab3/White.cs
@@ -1,4 +1,4 @@
-﻿using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Lab3
 {
@@ -9,7 +9,14 @@ namespace Lab3
             double averageHeight = 0;
 
             // code here
-
+            double s = 0;
+            for (int i = 0; i < n; i++)
+            {
+                double x;
+                x = Convert.ToDouble((Console.ReadLine()));
+                s += x;
+                averageHeight = s / n;
+            }
             // end
 
             return averageHeight;
@@ -19,7 +26,14 @@ namespace Lab3
             double bestResult = 0;
 
             // code here
-
+            double s = 0;
+            for (int i = 0; i < n; i++)
+            {
+                double x;
+                x = Convert.ToInt32((Console.ReadLine()));
+                if (s < x) s = x;
+            }
+            bestResult = s;
             // end
 
             return bestResult;
@@ -29,7 +43,12 @@ namespace Lab3
             int count = 0;
 
             // code here
-
+            for (int i = 0; i < n; i++)
+            {
+                double x;
+                x = Convert.ToDouble((Console.ReadLine()));
+                if (x <= limit) count++;
+            }
             // end
 
             return count;
@@ -39,7 +58,14 @@ namespace Lab3
             int hours = 0;
 
             // code here
-
+            int amount;
+            amount = Convert.ToInt32((Console.ReadLine()));
+            while (amount < maxAmount)
+            {
+                if (hours % 5 != 4) amount++;
+                               else amount -= 2;
+                hours++;
+            }
             // end
 
             return hours;
@@ -49,7 +75,9 @@ namespace Lab3
             double area = 0;
 
             // code here
-
+            if (type == 1) area = r * r;
+                else if (type == 2) area = Math.PI * r * r;
+                else if (type == 3) area = r * r * Math.Sqrt(3) / 4;
             // end
 
             return area;
