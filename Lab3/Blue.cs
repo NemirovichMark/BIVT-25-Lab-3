d4f6b3e04d8a3fa1606a779edcf8164eef9d3a9c
Ekaterina <grizhebovskayk@gmail.com>
2025-09-28 03:51:05 +0300

    Lab3 done

--- Lab3/Blue.cs
+++ Lab3/Blue.cs
@@ -7,7 +7,15 @@
             double milk = 0;
 
             // code here
-
+            for (int i = 0; i < n; i++)
+            {
+                double w = double.Parse(Console.ReadLine());
+                milk += (double)glass / 1000;
+                if (w < norma)
+                {
+                    milk += (double)glass / 1000;
+                }
+            }
             // end
 
             return milk;
@@ -17,7 +25,27 @@
             int first = 0, second = 0, third = 0, fourth = 0;
 
             // code here
-
+            for (int i = 0; i < n; i++) 
+            {
+                double x = double.Parse(Console.ReadLine());
+                double y = double.Parse(Console.ReadLine());
+                if (x > 0 && y > 0)
+                {
+                    first++;
+                }
+                else if (x > 0 && y < 0)
+                {
+                    second++;
+                }
+                else if (x < 0 && y < 0)
+                {
+                    third++;
+                }
+                else if (x < 0 && y > 0)
+                {
+                    fourth++;
+                }
+            }
             // end
 
             return (first, second, third, fourth);
@@ -27,7 +55,17 @@
             int count = 0;
 
             // code here
-
+            for (int i = 0; i < n; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                int c = int.Parse(Console.ReadLine());
+                int d = int.Parse(Console.ReadLine());
+                if (a > 3 && b > 3 && c > 3 && d > 3)
+                {
+                    count++;
+                }
+            }
             // end
 
             return count;
@@ -37,16 +75,66 @@
             int serias = 0;
 
             // code here
+            int seriasTime, taskTime = 10;
 
-            // end
+            while (time < 1440)
+            {
+                if (tasks > 0)
+                {
+                    time += taskTime;
+                    taskTime += 5;
+                    tasks--;
+                }
+                else
+                {
+                    seriasTime = int.Parse(Console.ReadLine());
+                    time += seriasTime;
+                    serias++;
+                }
+            }
+                // end
 
-            return (tasks, serias);
+                return (tasks, serias);
         }
         public (int power, int agility, int intellect) Task5(int power, int agility, int intellect, int number)
         {
 
             // code here
+            if (number == 1 || number == 3)
+            {
+                power += 10;
+                intellect -= 5;
+            }
+            else if (number == 2)
+            {
+                agility += 5;
+                power -= 5;
+                intellect -= 5;
+            }
+            else if (number == 4)
+            {
+                agility += 15;
+                power -= 10;
+                intellect -= 10;
+            }
+            else if (number == 5)
+            {
+                intellect += 7;
+                power -= 5;
+            }
 
+            if (power < 0)
+            {
+                power = 0;
+            }
+            if (intellect < 0)
+            {
+                intellect = 0;
+            }
+            if (agility < 0)
+            {
+                agility = 0;
+            }
             // end
 
             return (power, agility, intellect);
