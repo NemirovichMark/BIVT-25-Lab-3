fe1c380a1192c3d5f2fc04e2b84d6d4c44acd162
geceokue <sivtseva07@inbox.ru>
2025-10-01 02:55:42 +0300

    Update White.cs

--- Lab3/White.cs
+++ Lab3/White.cs
@@ -1,4 +1,4 @@
-﻿using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Lab3
 {
@@ -7,52 +7,81 @@ namespace Lab3
         public double Task1(int n)
         {
             double averageHeight = 0;
-
-            // code here
-
-            // end
-
+            int s = 0;
+            for(int i = 0; i < n; i++)
+            {
+                int Height = int.Parse(Console.ReadLine());
+                s += Height;
+            }
+            averageHeight = s / n;
             return averageHeight;
         }
         public double Task2(int n)
         {
             double bestResult = 0;
-
-            // code here
-
-            // end
-
+            int s = 0;
+            for(int i = 0; i<n; i++)
+            {
+                int Result = int.Parse(Console.ReadLine());
+                if(s < Result)
+                {
+                    s = Result;
+                }
+            }
+            bestResult = s;
             return bestResult;
         }
         public int Task3(int n, double limit)
         {
             int count = 0;
-
-            // code here
-
-            // end
-
+            for(int i = 0; i < n; i++)
+            {
+                int Time = int.Parse(Console.ReadLine());
+                if(Time >= limit)
+                {
+                    count++;
+                }
+            }
             return count;
         }
         public int Task4(int maxAmount)
         {
             int hours = 0;
-
-            // code here
-
-            // end
-
-            return hours;
+            int amount = int.Parse(Console.ReadLine());
+            if(amount > maxAmount)
+            {
+                if(amount % 5 != 4)
+                {
+                    amount += 1;
+                }
+                else
+                {
+                    amount -= 2;
+                }
+                hours++;
+            }
+            else
+            {
+                return hours;
+            }
+                return hours;
         }
         public double Task5(int r, int type)
         {
             double area = 0;
-
-            // code here
-
-            // end
-
-            return area;
+            if(type == 1)
+            {
+                area = r * r;
+            }
+            else if(type == 2)
+            {
+                area = Math.PI * r * r;
+            }
+            else if(type == 3)
+            {
+                area = (Math.Sqrt(3) / 4) * (r * r);
+            }
+                return area;
         }
     }
 }
