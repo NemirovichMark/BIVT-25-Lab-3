e75cd747cd3d09182b311483eaec7b2ffc9e18a6
Alohamod <airat.grafon@gmail.com>
2025-09-30 17:30:41 +0300

    Update Green.cs

--- Lab3/Green.cs
+++ Lab3/Green.cs
@@ -1,4 +1,4 @@
-﻿namespace Lab3
+namespace Lab3
 {
     public class Green
     {
@@ -7,7 +7,15 @@
             int count = 0;
 
             // code here
-
+            for (int i = 0; i < n; i++)
+            {
+                double x = int.Parse(Console.ReadLine());
+                double y = int.Parse(Console.ReadLine());
+                if ((x-a)*(x-a) +(y-b)*(y-b) <= r*r)
+                {
+                    count++;
+                }
+            }
             // end
 
             return count;
@@ -18,7 +26,17 @@
             double length = 0;
 
             // code here
-
+            length = 10000;
+            for (int i = 1; i <= n; i++)
+            {
+                double x = int.Parse(Console.ReadLine());
+                double y = int.Parse(Console.ReadLine());
+                if (Math.Sqrt(x * x + y * y) < length)
+                {
+                    index = i;
+                    length = Math.Sqrt(x * x + y * y);
+                }
+            }
             // end
 
             return (index, length);
@@ -28,7 +46,14 @@
             int count = 0;
 
             // code here
-
+            while (true)
+            {
+                double x, y;
+                bool x1_bool = Double.TryParse(Console.ReadLine(), out x);
+                bool y1_bool = Double.TryParse(Console.ReadLine(), out y);
+                if (x1_bool == false || y1_bool == false) break;
+                if ((x >= 0) && (x <= Math.PI) && (Math.Sin(x) >= y)) count++;
+            }
             // end
 
             return count;
@@ -38,6 +63,20 @@
             int score = 0;
 
             // code here
+            while (labs >0 || cw >0)
+            {
+                int mark = Int32.Parse(Console.ReadLine());
+                if (labs > 0)
+                {
+                    score += mark;
+                    labs--;
+                }
+                else
+                {
+                    score *= (4 * mark);
+                    cw--;
+                }
+            }
 
             // end
 
@@ -48,7 +87,21 @@
             double area = 0;
 
             // code here
+            switch(type)
+            {
+                case 1:
+                    area = a * b;
+                    break;
+                    
+                case 2:
+                    area = Math.Abs(Math.PI*(a*a - b*b));
+                    break;
 
+                case 3:
+                    area = 0.25*a*Math.Sqrt(4*b*b - (a*a));
+                    break;
+
+            }
             // end
 
             return area;
