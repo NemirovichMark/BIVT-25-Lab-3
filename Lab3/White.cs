8ce095a5bc44e35804549dbc7efd0a7d4f4002f1
Arina3004 <arina.maksimova2007@mail.ru>
2025-10-01 23:13:20 +0300

    White

--- Lab3/White.cs
+++ Lab3/White.cs
@@ -1,58 +1,126 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.ComponentModel.Design;
+using System.IO.Pipes;
+using System.Runtime.InteropServices;
 
-namespace Lab3
+namespace Lab1
 {
     public class White
     {
-        public double Task1(int n)
+        public bool Task1(double d)
         {
-            double averageHeight = 0;
+            bool answer = false;
 
             // code here
+            if (d > 0) answer = true;
+            else answer = false;
 
             // end
 
-            return averageHeight;
+            return answer;
         }
-        public double Task2(int n)
+        public bool Task2(int n)
         {
-            double bestResult = 0;
+            bool answer = false;
 
             // code here
+            if (n % 2 == 0) answer = true;
+            else answer = false;
 
             // end
 
-            return bestResult;
+            return answer;
         }
-        public int Task3(int n, double limit)
+        public int Task3(int a, int b)
         {
-            int count = 0;
+            int answer = 0;
 
             // code here
+            if (a > b) answer = a;
+            else answer = b;
+            
 
             // end
 
-            return count;
+            return answer;
         }
-        public int Task4(int maxAmount)
+        public double Task4(double d, double f)
         {
-            int hours = 0;
+            double answer = 0;
 
             // code here
-
+            double a = Math.Abs(f);
+            double b = Math.Abs(d);
+            if (a < b) answer = f;
+            else
+            {
+                answer = d;
+            }
             // end
 
-            return hours;
+            return answer;
         }
-        public double Task5(int r, int type)
+        public double Task5(double x)
         {
-            double area = 0;
+            double answer = 0;
 
             // code here
+            if (Math.Abs(x) > 1) answer = 1;
+            else answer = x;
+
+                // end
+
+                return answer;
+        }
+        public bool Task6(double x, double y, double r)
+        {
+            bool answer = false;
+
+            // code here
+            double vir = x * x + y * y - r * r;
+            if (Math.Abs(vir) <= Math.Pow(10, -6) || Math.Abs(vir) <= r * r * 1e-4)
+            {
+                answer = true;
+            }
+            // end
+
+            return answer;
+        }
+
+        public bool Task7(int n)
+        {
+                
+            // code here
+            int s = n * n;
+            bool answer = false;
+            if (s - n > 2 * n) 
+            { 
+                if (n % 2 == 0)
+                {
+                    answer = true;
+                }
+            }
+            return answer;
 
             // end
 
-            return area;
+        }
+        public bool Task8(double L, int T, int M)
+        {
+            bool answer = false;
+
+            // code here
+            if (L / 10 <= 3)
+            {
+                if (T+M >= 5)
+                {
+                    if ( M % 2 == 0)
+                        answer = true;
+                }
+                
+            }   
+            // end
+
+            return answer;
         }
     }
 }
