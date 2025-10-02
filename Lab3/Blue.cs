8a22567c5a3bc9ab1cb5303179f308673103b0b4
Маштак Игорь <io.mashtak@yandex.ru>
2025-10-02 09:30:10 +0300

    Add files via upload

--- Lab3/Blue.cs
+++ Lab3/Blue.cs
@@ -8,6 +8,16 @@
 
             // code here
 
+            for (int i = 0; i < n; i++)
+            {
+                double.TryParse(Console.ReadLine(), out double w);
+                if (w < norma)
+                {
+                    milk += glass;
+                }
+            }
+            milk /= 1000;
+
             // end
 
             return milk;
@@ -17,6 +27,34 @@
             int first = 0, second = 0, third = 0, fourth = 0;
 
             // code here
+
+            for (int i = 0; i < n; i++)
+            {
+                double.TryParse(Console.ReadLine(), out double x);
+                double.TryParse(Console.ReadLine(), out double y);
+                if (y > 0)
+                {
+                    if (x > 0)
+                    {
+                        first++;
+                    }
+                    else if (x < 0)
+                    {
+                        second++;
+                    }
+                }
+                else if (y < 0)
+                {
+                    if (x < 0)
+                    {
+                        third++;
+                    }
+                    else if (x > 0)
+                    {
+                        fourth++;
+                    }
+                }
+            }
 
             // end
 
@@ -28,6 +66,19 @@
 
             // code here
 
+            for (int i = 0; i < n; ++i)
+            {
+                int.TryParse(Console.ReadLine(), out int a);
+                int.TryParse(Console.ReadLine(), out int b);
+                int.TryParse(Console.ReadLine(), out int c);
+                int.TryParse(Console.ReadLine(), out int d);
+                int min_mark = Math.Min(Math.Min(a, b), Math.Min(c, d));
+                if (min_mark >= 4)
+                {
+                    count++;
+                }
+            }
+
             // end
 
             return count;
@@ -38,6 +89,24 @@
 
             // code here
 
+            int seriasTime, taskTime = 10;
+
+            while (time < 24 * 60)
+            {
+                if (tasks > 0)
+                {
+                    time += taskTime;
+                    taskTime += 5;
+                    tasks--;
+                }
+                else
+                {
+                    int.TryParse(Console.ReadLine(), out seriasTime);
+                    time += seriasTime;
+                    serias++;
+                }
+            }
+
             // end
 
             return (tasks, serias);
@@ -46,6 +115,35 @@
         {
 
             // code here
+
+            switch(number)
+            {
+                case 1:
+                    power += 10;
+                    intellect -= 5;
+                    break;
+                case 2:
+                    power -= 5;
+                    agility += 5;
+                    intellect -= 5;
+                    break;
+                case 3:
+                    power += 10;
+                    intellect -= 5;
+                    break;
+                case 4:
+                    power -= 10;
+                    agility += 15;
+                    intellect -= 10;
+                    break;
+                case 5:
+                    power -= 5;
+                    intellect += 7;
+                    break;
+            }
+            power = Math.Max(power, 0);
+            agility = Math.Max(agility, 0);
+            intellect = Math.Max(intellect, 0);
 
             // end
 
