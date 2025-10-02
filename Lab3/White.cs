44f283ef76eed6a513f29dfbb9bd6bae6df65165
Vlad-Bond17 <jacksonfigerald@gmail.com>
2025-10-02 22:45:00 +0300

    Add files via upload

--- Lab3/White.cs
+++ Lab3/White.cs
@@ -9,7 +9,14 @@ namespace Lab3
             double averageHeight = 0;
 
             // code here
-
+            double summ = 0;
+            for (int i = 0; i < n; i++)
+            {
+                double x;
+                x = Convert.ToDouble((Console.ReadLine()));
+                summ = summ + x;
+                averageHeight = summ / n;
+            }
             // end
 
             return averageHeight;
@@ -19,7 +26,23 @@ namespace Lab3
             double bestResult = 0;
 
             // code here
-
+            bestResult = double.MaxValue;
+            if (n <= 0)
+            {
+                bestResult = 0;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                double time = double.Parse(Console.ReadLine());
+                if (time == 0)
+                {
+                    bestResult = 0;
+                }
+                if (time < bestResult)
+                {
+                    bestResult = time;
+                }
+            }
             // end
 
             return bestResult;
@@ -29,7 +52,18 @@ namespace Lab3
             int count = 0;
 
             // code here
-
+            if (n == 0)
+            {
+                count = 0;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                double time = double.Parse(Console.ReadLine());
+                if (time <= limit)
+                {
+                    count++;
+                }
+            }
             // end
 
             return count;
@@ -39,6 +73,19 @@ namespace Lab3
             int hours = 0;
 
             // code here
+            int amount = int.Parse(Console.ReadLine());
+            while(amount < maxAmount)
+            {
+                if (hours % 5 != 4)
+                {
+                    amount += 1;
+                }
+                else
+                {
+                    amount -= 2;
+                }
+                hours++;
+            }
 
             // end
 
@@ -49,10 +96,21 @@ namespace Lab3
             double area = 0;
 
             // code here
+            if (type == 1)
+            {
+                area = r * r;
+            }
+            else if (type == 2)
+            {
+                area = Math.PI * r * r;
+            }
+            else if (type == 3)
+            {
+                area = r * r * Math.Sqrt(3) / 4;
+            }
+                // end
 
-            // end
-
-            return area;
+                return area;
         }
     }
 }
