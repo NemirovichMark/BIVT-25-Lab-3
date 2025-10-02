3ca56eac793d1619254aecd7efd05f8bc57af9f0
Илья Киселёв <138257294+nakakorik@users.noreply.github.com>
2025-10-02 19:59:11 +0200

    Add files via upload

--- Lab3/Blue.cs
+++ Lab3/Blue.cs
@@ -1,4 +1,6 @@
-﻿namespace Lab3
+﻿using System.Text.RegularExpressions;
+
+namespace Lab3
 {
     public class Blue
     {
@@ -62,7 +64,14 @@
             // code here
             for(int i = 0; i < n; i++)
             {
-
+                int grade_1 = int.Parse(Console.ReadLine());
+                int grade_2 = int.Parse(Console.ReadLine());
+                int grade_3 = int.Parse(Console.ReadLine());
+                int grade_4 = int.Parse(Console.ReadLine());
+                if (grade_1 > 3 && grade_2 > 3 && grade_3 > 3 && grade_4 > 3)
+                {
+                    count++;
+                }
             }
             // end
 
@@ -75,16 +84,60 @@
             // code here
             int seriasTime = 10;
             int taskTime = 10;
+            while (time < 1440)
+            {
+                if (tasks > 0)
+                {
+                    time += taskTime;
+                    taskTime += 5;
+                    tasks--;
 
-            // end
+                }
+                else
+                {
+                    int.TryParse(Console.ReadLine(), out seriasTime);
+                    time += seriasTime;
+                    serias++;
 
-            return (tasks, serias);
+                }
+
+                // end
+            }
+                    return (tasks, serias);
         }
         public (int power, int agility, int intellect) Task5(int power, int agility, int intellect, int number)
         {
 
             // code here
+            switch (number)
+            {
+                case 1:
+                    power += 10;
+                    intellect -= 5;
+                    break;
+                case 2:
+                    agility += 5;
+                    power -= 5;
+                    intellect -= 5;
+                    break;
+                case 3:
+                    power += 10;
+                    intellect -= 5;
+                    break;
+                case 4:
+                    agility += 15;
+                    intellect -= 10;
+                    power -= 10;
+                    break;
+                case 5:
+                    intellect += 7;
+                    power -= 7;
+                    break;
 
+            }
+            if (power < 0) power = 0;
+            if (agility < 0) agility = 0;
+            if (intellect < 0) intellect = 0;
             // end
 
             return (power, agility, intellect);
