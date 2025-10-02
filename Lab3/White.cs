3d95edf5a07c702d01361395bc31a87abb9158a8
pprudnikova10-stack <pprudnikova10@gmail.com>
2025-10-02 23:22:33 +0300

    Add files via upload

--- Lab3/White.cs
+++ Lab3/White.cs
@@ -7,40 +7,69 @@ namespace Lab3
         public double Task1(int n)
         {
             double averageHeight = 0;
-
-            // code here
-
-            // end
+            int sum = 0;
+            for(int i = 0; i < n; i++)
+            {
+                sum += int.Parse(Console.ReadLine());
+                
+            }
+            if (n != 0)
+            {
+                averageHeight = (double) sum / n;
+            }
+            
 
             return averageHeight;
         }
         public double Task2(int n)
         {
-            double bestResult = 0;
-
-            // code here
-
-            // end
+            if (n == 0) return 0;
+            double bestResult = double.Parse(Console.ReadLine());
+            double result = 0;
+            for(int i = 0; i < n - 1; i++)
+            {
+                result = double.Parse(Console.ReadLine());
+                if(bestResult > result)
+                {
+                    bestResult = result;
+                   
+                }
+            }
+            
 
             return bestResult;
         }
         public int Task3(int n, double limit)
         {
+            if (n == 0) return 0;
             int count = 0;
-
-            // code here
-
-            // end
-
+            double result = 0;
+            for(int i = 0; i < n; i++)
+            {
+                result = double.Parse(Console.ReadLine());
+                if(result <= limit)
+                {
+                    count++;
+                }
+            }
             return count;
         }
         public int Task4(int maxAmount)
         {
             int hours = 0;
-
-            // code here
-
-            // end
+            int amount = int.Parse(Console.ReadLine());
+            while(amount < maxAmount)
+            {
+                if(hours % 5 != 4)
+                {
+                    amount += 1;
+                }
+                else
+                {
+                    amount -= 2;
+                }
+                hours++;
+            }
 
             return hours;
         }
@@ -48,9 +77,21 @@ namespace Lab3
         {
             double area = 0;
 
-            // code here
+            switch (type)
+            {
+                case 1:
+                    area = r * r;
+                    break;
+                case 2:
+                    area = Math.PI * r * r;
+                    break;
+                case 3:
+                    area = r * r * Math.Sqrt(3) / 4;
+                    break;
+                default:
+                    break;
 
-            // end
+            }
 
             return area;
         }
