b647b59056f84e921719a1e064449b5a679b2069
mark-v4 <markovkaplay101@gmail.com>
2025-10-04 01:18:57 +0300

    solved

--- Lab3/Purple.cs
+++ Lab3/Purple.cs
@@ -10,6 +10,18 @@ namespace Lab3
 
             // code here
 
+            double x, y;
+            while (n-- > 0)
+            {
+                double.TryParse(Console.ReadLine(), out x);
+                double.TryParse(Console.ReadLine(), out y);
+                double d = x * x + y * y;
+                if (r1 * r1 <= d && d <= r2 * r2)
+                {
+                    count++;
+                }
+            }
+
             // end
 
             return count;
@@ -21,6 +33,24 @@ namespace Lab3
 
             // code here
 
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int.TryParse(Console.ReadLine(), out int a);
+                int.TryParse(Console.ReadLine(), out int b);
+                int.TryParse(Console.ReadLine(), out int c);
+                int.TryParse(Console.ReadLine(), out int d);
+                if (a == 2 || b == 2 || c == 2 || d == 2)
+                {
+                    count++;
+                }
+                sum += a + b + c + d;
+            }
+            if (n != 0)
+            {
+                average = sum / (double)n / 4;
+            }
+
             // end
 
             return (count, average);
@@ -30,6 +60,25 @@ namespace Lab3
             double avgMark = 0;
 
             // code here
+
+            int th, pr, mark, n = exams;
+            double sc;
+            while (exams > 0)
+            {
+                int.TryParse(Console.ReadLine(), out th);
+                int.TryParse(Console.ReadLine(), out pr);
+                sc = 0.4 * th + 0.6 * pr;
+                if (sc > 85)
+                    mark = 5;
+                else if (sc > 70)
+                    mark = 4;
+                else if (sc > 50)
+                    mark = 3;
+                else
+                    mark = 2;
+                avgMark += (double)mark / n;
+                exams--;
+            }
 
             // end
 
@@ -42,6 +91,25 @@ namespace Lab3
 
             // code here
 
+            while (attempts < limit)
+            {
+                int.TryParse(Console.ReadLine(), out int a);
+                int.TryParse(Console.ReadLine(), out int b);
+                int.TryParse(Console.ReadLine(), out int c);
+                attempts++;
+                if (a == -1 || b == -1 || c == -1)
+                {
+                    solution = "Аварийный выход!";
+                    return (solution, attempts);
+                }
+                if (a == code / 100 && b == code / 10 % 10 && c == code % 10)
+                {
+                    solution = "Доступ разрешен!";
+                    return (solution, attempts);
+                }
+            }
+            solution = "Система заблокирована!";
+
             // end
 
             return (solution, attempts);
@@ -51,6 +119,36 @@ namespace Lab3
             double luck = 0;
 
             // code here
+
+            while (n-- > 0)
+            {
+                switch (a) {
+                    case 1:
+                    case 8:
+                    case 15:
+                    case 22:
+                    case 29:
+                        luck = Math.Min(luck * 1.5, 100);
+                        break;
+                    case 4:
+                    case 11:
+                    case 18:
+                    case 25:
+                        luck = Math.Max(luck - 10, 0);
+                        break;
+                    case 7:
+                    case 14:
+                    case 21:
+                    case 28:
+                        if (luck < 50)
+                            luck = 55;
+                        break;
+                    default:
+                        luck = Math.Min(luck + 5, 100);
+                        break;
+                }
+                a++;
+            }
 
             // end
 
