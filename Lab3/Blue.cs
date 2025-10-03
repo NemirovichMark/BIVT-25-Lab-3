49a8c105f002961b2c808ba499553b1e59d36a19
chelovekm <161503290+chelovekm@users.noreply.github.com>
2025-10-03 13:46:48 +0300

    Update Blue.cs

--- Lab3/Blue.cs
+++ Lab3/Blue.cs
@@ -1,4 +1,6 @@
-﻿namespace Lab3
+using System.Net.Http.Headers;
+
+namespace Lab3
 {
     public class Blue
     {
@@ -7,18 +9,45 @@
             double milk = 0;
 
             // code here
+            for (int i = 0; i < n; i++)
+            {
+                double s = double.Parse(Console.ReadLine());
+                if (s < norma){
+                    milk += glass/1000.0;
+                }
 
+            }
+            milk = Math.Round(milk, 3);
             // end
-
             return milk;
         }
         public (int first, int second, int third, int fourth) Task2(int n)
         {
             int first = 0, second = 0, third = 0, fourth = 0;
 
-            // code here
-
-            // end
+            //code here
+            for (int i = 0; i < n; i++)
+            {
+                double x = double.Parse(Console.ReadLine());
+                double y = double.Parse(Console.ReadLine());
+                if (x > 0 && y > 0)
+                {
+                    first += 1;
+                }
+                else if (x > 0 && y < 0)
+                {
+                    fourth += 1;
+                }
+                else if (x < 0 && y > 0)
+                {
+                    second += 1;
+                }
+                else if (x < 0 && y < 0)
+                {
+                    third += 1;
+                }
+            }
+            //end
 
             return (first, second, third, fourth);
         }
@@ -27,7 +56,25 @@
             int count = 0;
 
             // code here
-
+            for (int i = 0; i < n; i++)
+            {
+                bool fl = false;
+                for (int j = 0; j < 4; j++)
+                {
+                    int grade = int.Parse(Console.ReadLine());
+                    switch (grade)
+                    {
+                        case 2:
+                        case 3:
+                            fl = true;
+                            break;
+                    }
+                }
+                if (!fl)
+                {
+                    count ++;
+                }
+            }
             // end
 
             return count;
@@ -37,7 +84,23 @@
             int serias = 0;
 
             // code here
+            int seriasTime, taskTime = 10;
 
+            while (time < 24 * 60)
+            {
+                if (tasks > 0)
+                {
+                    time += taskTime;
+                    taskTime += 5;
+                    tasks--;
+                }
+                else
+                {
+                    seriasTime = Convert.ToInt32(Console.ReadLine());
+                    time += seriasTime;
+                    serias++;
+                }
+            }
             // end
 
             return (tasks, serias);
@@ -46,7 +109,33 @@
         {
 
             // code here
-
+            if (number == 1)
+            {
+                power += 10;
+                intellect = Math.Max(intellect - 5, 0);
+            }
+            else if (number == 2)
+            {
+                agility += 5;
+                power = Math.Max(power - 5, 0);
+                intellect = Math.Max(intellect - 5, 0);
+            }
+            else if (number == 3)
+            {
+                power += 10;
+                intellect = Math.Max(intellect - 5, 0);
+            }
+            else if (number == 4)
+            {
+                agility += 15;
+                power = Math.Max(power - 10, 0);
+                intellect = Math.Max(intellect - 10, 0);
+            }
+            else if (number == 5) 
+            {
+                intellect += 7;
+                power = Math.Max(power - 5, 0);
+            }
             // end
 
             return (power, agility, intellect);
