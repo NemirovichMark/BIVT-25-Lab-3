f562ce6ea8a9d20cf05f83c6cff0e247c5752ed6
WoblaSM <118443431+WoblaSM@users.noreply.github.com>
2025-10-03 11:41:54 +0300

    Update Blue.cs

--- Lab3/Blue.cs
+++ Lab3/Blue.cs
@@ -13,7 +13,7 @@ namespace Lab3
             for (int i = 0; i != n; i++)
             {
                 Console.Write("вес ученика: ");
-                int wes = Convert.ToInt32(Console.ReadLine());
+                double wes = Convert.ToDouble(Console.ReadLine());
                 if (wes < norma)
                 {
                     milk += glass;
@@ -32,28 +32,79 @@ namespace Lab3
             int s2 = second;
             int s3 = third;
             int s4 = fourth;
-
+            List<string> k1 = new List<string>();
+            List<string> k2 = new List<string>();
+            List<string> k3 = new List<string>();
+            List<string> k4 = new List<string>();
             // code here
             for (int i = 0; i != n; i++)
             {
-                Console.WriteLine("x = ");
-                int x = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("y = ");
-                int y = Convert.ToInt32(Console.ReadLine());
-                if (x > 0 & y > 0) s1 += 1;
-                if (x < 0 & y < 0) s2 += 1;
-                if (x < 0 & y > 0) s3 += 1;
-                if (x > 0 & y < 0) s4 += 1;
-                
+                Console.Write("x = ");
+                double x = Convert.ToDouble(Console.ReadLine());
+                Console.Write("y = ");
+                double y = Convert.ToDouble(Console.ReadLine());
+                if (x > 0 && y > 0){ 
+                    s1 += 1;
+                    k1.Add(Convert.ToString(x) + " " + Convert.ToString(y));
+                }
+                if (x < 0 && y < 0)
+                {
+                    s2 += 1;
+                    k2.Add(Convert.ToString(x) + " " + Convert.ToString(y));
+                }
+                if (x < 0 && y > 0)
+                {
+                    s3 += 1;
+                    k3.Add(Convert.ToString(x) + " " + Convert.ToString(y));
+                }
+                if (x > 0 && y < 0)
+                {
+                    s4 += 1;
+                    k4.Add(Convert.ToString(x) + " " + Convert.ToString(y));
+                }
+
 
             }
-            Console.WriteLine("1-й = " + s1);
-            Console.WriteLine("2-й = " + s3);
-            Console.WriteLine("3-й = " + s2);
-            Console.WriteLine("4-й = " + s4);
+            //Console.WriteLine("1-й = " + k1.Distinct().Count());
+            Console.WriteLine(s1);
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach (var item in k1.Distinct())
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(s3);
+            //Console.WriteLine("2-й = " + k3.Distinct().Count());
+            foreach (var item in k3.Distinct())
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(s2);
+            //Console.WriteLine("3-й = " + k2.Distinct().Count());
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach (var item in k2.Distinct())
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(s4);
+            //Console.WriteLine("4-й = " + k4.Distinct().Count());
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach (var item in k4.Distinct())
+            {
+                Console.WriteLine(item);
+            }
+
             first = s1;
-            second = s2;
-            third = s3;
+            second = s3;
+            third = s2;
             fourth = s4;
 
             // end
