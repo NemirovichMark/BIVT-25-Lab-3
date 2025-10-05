23045afe0cd23e7d6b0eb2f50520beddaa17b4c6
Евгения <jenekrasilnikova@gmail.com>
2025-10-05 19:51:10 +0300

    Update Green.cs

--- Lab3/Green.cs
+++ Lab3/Green.cs
@@ -1,4 +1,4 @@
-﻿namespace Lab3
+namespace Lab3
 {
     public class Green
     {
@@ -7,6 +7,17 @@
             int count = 0;
 
             // code here
+            for (int i = 0; i < n; i++)
+            {
+                double x = double.Parse(Console.ReadLine());
+                double y = double.Parse(Console.ReadLine());
+                if ((x - a) * (x - a) + (y - b) * (y - b) <= r * r)
+                {
+                    count++;
+                }
+            }
+            
+            
 
             // end
 
@@ -18,6 +29,19 @@
             double length = 0;
 
             // code here
+            double m = 10005000000000000000;
+            for (int i = 0; i < n; i++)
+            {
+                double x = double.Parse(Console.ReadLine());
+                double y = double.Parse(Console.ReadLine());
+                if (Math.Sqrt(x * x + y * y) <= m)
+                {
+                    m = Math.Sqrt(x * x + y * y);
+                    index = i + 1;
+                    length = m;
+                }
+                
+            }
 
             // end
 
@@ -28,6 +52,14 @@
             int count = 0;
 
             // code here
+            
+            while(double.TryParse(Console.ReadLine(), out double x) && double.TryParse(Console.ReadLine(), out double y))
+            {
+                if (x >= 0 && x <= Math.PI && y >= 0 && y <= Math.Sin(x))
+                {
+                    count ++;
+                }
+            }
 
             // end
 
@@ -38,16 +70,46 @@
             int score = 0;
 
             // code here
+            while (labs > 0 || cw > 0)
+            {
+                int mark = int.Parse(Console.ReadLine());
+                if (labs > 0)
+                {
+                    score += mark;
+                    labs--;
+                }
+                else
+                {
+                    score += 4 * mark;
+                    cw--;
+                }
+            }
 
+        }
             // end
 
             return score;
-        }
         public double Task5(int a, int b, int type)
         {
             double area = 0;
 
             // code here
+            if (type == 1)
+            {
+                area = a * b;
+            }
+            else if (type == 2)
+            {
+                area = Math.PI * Math.Abs(( (a*a) - (b*b) ));
+            }
+            else if (type == 3)
+            {
+                double p = (a + b + b) / 2.0;
+                area = Math.Sqrt(p * (p - a) * (p - b) * (p - b));
+            }
+            
+            
+            
 
             // end
 
