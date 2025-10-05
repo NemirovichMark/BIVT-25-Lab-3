7d69435463091dc44c94462075bd3c50e8ecdd54
harn <genyievg@gmail.com>
2025-10-05 17:16:08 +0300

    all_tasks

--- Lab3/Green.cs
+++ Lab3/Green.cs
@@ -5,8 +5,20 @@
         public double Task1(int a, int b, int r, int n)
         {
             int count = 0;
-
+            
+            double r_pow = r * r;
             // code here
+            for (int i = 0; i < n; i++)
+            {
+                double x = double.Parse(Console.ReadLine());
+                double y = double.Parse(Console.ReadLine());
+
+                double dx = x - a;
+                double dy = y - b;
+
+                if (dx * dx + dy * dy <= r_pow)
+                    count++;
+            }
 
             // end
 
@@ -19,6 +31,18 @@
 
             // code here
 
+            double lent = 1e10; 
+            for (int i = 0; i < n; i++)
+            {
+                double x = double.Parse(Console.ReadLine());
+                double y = double.Parse(Console.ReadLine());
+                if (x * x + y * y < lent)
+                {
+                    lent = x * x + y * y;
+                    length = Math.Sqrt(lent);
+                    index = i + 1;
+                }
+            }
             // end
 
             return (index, length);
@@ -28,7 +52,25 @@
             int count = 0;
 
             // code here
+            while (true)
+            {
+                string cord_x = Console.ReadLine();
+                if (!double.TryParse(cord_x, out double x))
+                {
+                    break;
+                }
 
+                string cord_y = Console.ReadLine();
+                if (!double.TryParse(cord_y, out double y))
+                {
+                    break;
+                }
+
+                if (x >= 0 && x <= Math.PI && y >= 0 && y <= Math.Sin(x))
+                {
+                    count++;
+                }
+            }
             // end
 
             return count;
@@ -38,7 +80,21 @@
             int score = 0;
 
             // code here
-
+            while (labs > 0 || cw > 0)
+            {
+                int.TryParse(Console.ReadLine(), out int mark);
+                if (labs > 0)
+                {
+                    score += mark;
+                    labs--;
+                }
+                else
+                {
+                    score += 4 * mark;
+                    cw--;
+                }
+            }
+            
             // end
 
             return score;
@@ -48,7 +104,25 @@
             double area = 0;
 
             // code here
+            switch (type)
+            {
+                case 1:
+                    area = a * b;
+                    break;
 
+                case 2:
+                    area = Math.Abs((Math.PI * a * a - Math.PI * b * b));
+                    break;
+
+                case 3:
+                    double p = (a + b + b) / 2.0;
+                    area = Math.Sqrt(p * (p - a) * (p - b) * (p - b));
+                    break;
+
+                default:
+                    area = 0;
+                    break;
+            }
             // end
 
             return area;
