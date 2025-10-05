ce2580b89930e9d73ec61c5db50dfed1310132fc
Abramova Ekaterina <m2503823@edu.misis.ru>
2025-10-05 15:34:46 +0300

    Add files via upload

--- Lab3/Green.cs
+++ Lab3/Green.cs
@@ -7,6 +7,17 @@
             int count = 0;
 
             // code here
+            for (int i = 0; i < n; i++)
+            {
+                string x = Console.ReadLine();
+                string y = Console.ReadLine();
+                double.TryParse(x, out double X);
+                double.TryParse(y, out double Y);
+                if ((X - a) * (X - a) + (Y - b) * (Y - b) <= r * r)
+                {
+                    count++;
+                }
+            }
 
             // end
 
@@ -18,7 +29,25 @@
             double length = 0;
 
             // code here
-
+            double.TryParse(Console.ReadLine(), out double mX);
+            double.TryParse(Console.ReadLine(), out double mY);
+            double mindist = Math.Sqrt(mX * mX + mY * mY);
+            int minind = 1;
+            for (int i=2;i<=n;i++)
+            {
+                string x = Console.ReadLine();
+                string y = Console.ReadLine();
+                double.TryParse(x, out double X);
+                double.TryParse(y, out double Y);
+                length = Math.Sqrt(X * X + Y * Y);
+                if (length < mindist)
+                {
+                    mindist = length;
+                    mX = X;
+                    mY = Y;
+                    minind = i;
+                }
+            }
             // end
 
             return (index, length);
@@ -28,10 +57,23 @@
             int count = 0;
 
             // code here
-
-            // end
-
-            return count;
+            while (true)
+            {
+                string x = Console.ReadLine();
+                string y = Console.ReadLine();
+                bool xtrue=double.TryParse(x, out double X);
+                bool ytrue=double.TryParse(y, out double Y);
+                if (Y >= 0 && Y <= Math.Sin(X) && X >= 0 && X <= Math.PI)
+                {
+                    count++;
+                }
+                else 
+                { 
+                    break; 
+                }
+                // end
+            }
+                return count;
         }
         public int Task4(int labs, int cw)
         {
