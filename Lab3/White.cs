4a5d4183dc994a42fdc6ed7d211241f00f64ee8f
ZolotovaPolina <p_zoloto.va@mail.ru>
2025-10-05 19:03:48 +0300

    Add files via upload

--- Lab3/White.cs
+++ Lab3/White.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Lab3
 {
@@ -9,9 +10,17 @@ namespace Lab3
             double averageHeight = 0;
 
             // code here
-
+            double r, s = 0;
+            int i = 0;
+            for (; ; i++)
+            {
+                double.TryParse(Console.ReadLine(), out r);
+                if (r <= 0) break;
+                s = s + r;
+            }
+            averageHeight = s / n;
             // end
-
+            //Console.WriteLine(averageHeight);
             return averageHeight;
         }
         public double Task2(int n)
@@ -20,6 +29,17 @@ namespace Lab3
 
             // code here
 
+            bestResult = double.MaxValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                double time = double.Parse(Console.ReadLine());
+                if (time < bestResult)
+                {
+                    bestResult = time;
+                }
+            }
+            Console.WriteLine(bestResult);
             // end
 
             return bestResult;
@@ -29,7 +49,12 @@ namespace Lab3
             int count = 0;
 
             // code here
-
+            double t = double.Parse(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                if (double.Parse(Console.ReadLine()) <= limit)
+                    count++;
+            }
             // end
 
             return count;
@@ -39,7 +64,23 @@ namespace Lab3
             int hours = 0;
 
             // code here
-
+            int hour = 0;
+            int amount = int.Parse(Console.ReadLine());
+            {
+                while (amount < maxAmount)
+                {
+                    if (hour % 5 != 4)
+                    {
+                        amount += 1;
+                        hours++;
+                    }
+                    else
+                    {
+                        amount -= 2;
+                        hours++;
+                    }
+                }
+            }
             // end
 
             return hours;
@@ -49,7 +90,23 @@ namespace Lab3
             double area = 0;
 
             // code here
+            switch (type)
+            {
+                case 1:
+                    area = r * r;
+                    break;
+                case 2:
+                    area = Math.PI * r * r;
+                    break;
+                case 3:
+                    area = (Math.Sqrt(3) / 4) * r * r;
+                    break;
+                default:
+                    Console.WriteLine("Неверный выбор. Выберите 1, 2 или 3:");
+                    break;
+            }
 
+            Console.WriteLine(area);
             // end
 
             return area;
