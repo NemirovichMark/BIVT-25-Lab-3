c367e5d8da27107738f5eadcde2727d6a970b310
SaweenSergey <svs0va@yandex.ru>
2025-10-05 11:44:16 +0300

    Add files via upload

--- Lab3/Blue.cs
+++ Lab3/Blue.cs
@@ -1,4 +1,6 @@
-﻿namespace Lab3
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Lab3
 {
     public class Blue
     {
@@ -7,7 +9,17 @@
             double milk = 0;
 
             // code here
-
+            for (int i = 0; i < n; i++)
+            {
+                string a = Console.ReadLine();
+                double w;
+                double.TryParse(a, out w);
+                if (w < norma)
+                {
+                    milk += glass;
+                }
+            }
+            milk /= 1000;
             // end
 
             return milk;
@@ -17,7 +29,31 @@
             int first = 0, second = 0, third = 0, fourth = 0;
 
             // code here
-
+            string a, b;
+            double x, y;
+            for (int i = 0; i < n; i++)
+            {
+                a = Console.ReadLine();
+                b = Console.ReadLine();
+                double.TryParse(a, out x);
+                double.TryParse(b, out y);
+                if ((x > 0) && (y > 0))
+                {
+                    first++;
+                }
+                else if ((x < 0) && (y > 0))
+                {
+                    second++;
+                }
+                else if ((x < 0) && (y < 0))
+                {
+                    third++;
+                }
+                else if ((x > 0) && (y < 0))
+                {
+                    fourth++;
+                }
+            }
             // end
 
             return (first, second, third, fourth);
@@ -27,7 +63,27 @@
             int count = 0;
 
             // code here
-
+            string a;
+            int IsNew = 0, f = 0, grade;
+            for (int i = 0; i < (n * 4); i++)
+            {
+                a = Console.ReadLine();
+                int.TryParse(a, out grade);
+                if ((grade == 2) || (grade == 3))
+                {
+                    f = 1;
+                }
+                IsNew++;
+                if (IsNew == 4)
+                {
+                    if (f == 0)
+                    {
+                        count += 1;
+                    }
+                    f = 0;
+                    IsNew = 0;
+                }
+            }
             // end
 
             return count;
@@ -37,7 +93,22 @@
             int serias = 0;
 
             // code here
-
+            int seriasTime, taskTime = 10;
+            while (time < 1440)
+            {
+                if (tasks > 0)
+                {
+                    time += taskTime;
+                    taskTime += 5;
+                    tasks--;
+                }
+                else
+                {
+                    int.TryParse(Console.ReadLine(), out seriasTime);
+                    time += seriasTime;
+                    serias++;
+                }
+            }
             // end
 
             return (tasks, serias);
@@ -46,7 +117,36 @@
         {
 
             // code here
-
+            if (number == 1)
+            {
+                power += 10;
+                intellect -= 5;
+            }
+            else if (number == 2)
+            {
+                agility += 5;
+                intellect -= 5;
+                power -= 5;
+            }
+            else if (number == 3)
+            {
+                power += 10;
+                intellect -= 5;
+            }
+            else if (number == 4)
+            {
+                agility += 15;
+                power -= 10;
+                intellect -= 10;
+            }
+            else if (number == 5)
+            {
+                intellect += 7;
+                power -= 5;
+            }
+            power = (power + Math.Abs(power)) / 2;
+            agility = (agility + Math.Abs(agility)) / 2;
+            intellect = (intellect + Math.Abs(intellect)) / 2;
             // end
 
             return (power, agility, intellect);
