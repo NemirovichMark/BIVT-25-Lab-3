15810f0cd4b60b336f8352ad187f75a67c7b1141
kirtimmit9 <kirtimmit@outlook.com>
2025-10-05 12:37:11 +0300

    Update Blue.cs

--- Lab3/Blue.cs
+++ Lab3/Blue.cs
@@ -1,4 +1,6 @@
-﻿namespace Lab3
+using System;
+
+namespace Lab3
 {
     public class Blue
     {
@@ -7,7 +9,17 @@
             double milk = 0;
 
             // code here
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("");
+                int weight = int.Parse(Console.ReadLine());
 
+                if (weight < norma)
+                {
+                    milk += glass;
+                }
+            }
+            milk = milk / 1000;
             // end
 
             return milk;
@@ -17,7 +29,29 @@
             int first = 0, second = 0, third = 0, fourth = 0;
 
             // code here
-
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"");
+                double x = double.Parse(Console.ReadLine());
+                Console.Write($"");
+                double y = double.Parse(Console.ReadLine());
+                if (x > 0 && y > 0)
+                {
+                    first++;
+                }
+                else if (x < 0 && y > 0)
+                {
+                    second++;
+                }
+                else if (x < 0 && y < 0)
+                {
+                    third++;
+                }
+                else if (x > 0 && y < 0)
+                {
+                    fourth++;
+                }
+            }
             // end
 
             return (first, second, third, fourth);
@@ -27,7 +61,23 @@
             int count = 0;
 
             // code here
-
+            for (int i = 0; i < n; i++)
+            {
+                bool neud = false;
+                for (int k = 0; k < 4; k++)
+                {
+                    Console.Write($"");
+                    int ud = int.Parse(Console.ReadLine());
+                    if (ud == 3 || ud == 2)
+                    {
+                        neud = true;
+                    }
+                }
+                if (neud==false)
+                {
+                    count++;
+                }
+            }
             // end
 
             return count;
@@ -37,7 +87,26 @@
             int serias = 0;
 
             // code here
+            int seriasTime = int.Parse(Console.ReadLine());
+            int taskTime = 10;
+            
 
+            while (time<24*60)
+            {
+                if (tasks > 0)
+                {
+                    time += taskTime;
+                    taskTime += 5;
+                    tasks--;
+                }
+                else 
+                {
+                    Console.Write("");
+                    seriasTime = int.Parse(Console.ReadLine());
+                    time += seriasTime;
+                    serias++;
+                }               
+            }
             // end
 
             return (tasks, serias);
@@ -46,7 +115,34 @@
         {
 
             // code here
-
+            switch (number)
+            {
+                case 1:
+                    power += 10;
+                    intellect -= 5;
+                    break;
+                case 2:
+                    agility += 5;
+                    power -= 5;
+                    intellect -= 5;
+                    break;
+                case 3:
+                    power += 10;
+                    intellect -= 5;
+                    break;
+                case 4:
+                    agility += 15;
+                    power -= 10;
+                    intellect -= 10;
+                    break;
+                case 5:
+                    intellect += 7;
+                    power -= 5;
+                    break;
+            }
+            power = Math.Max(power, 0);
+            agility = Math.Max(agility, 0);
+            intellect = Math.Max(intellect, 0);
             // end
 
             return (power, agility, intellect);
