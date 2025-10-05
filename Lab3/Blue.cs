96a38fdc23425ca9dc68213a6c0a2395efdd42ac
liza-127 <lizadaystar12737@gmail.com>
2025-10-05 16:03:42 +0300

    Update Blue.cs

--- Lab3/Blue.cs
+++ Lab3/Blue.cs
@@ -7,6 +7,18 @@
             double milk = 0;
 
             // code here
+            for (int i = 0; i < n; i++)
+            {
+                double s = glass;
+                double ves = Convert.ToDouble(Console.ReadLine());
+                if (ves < norma)
+                {
+                    milk+= glass;
+                }
+
+                
+            }
+            milk = milk / 1000.0;
 
             // end
 
@@ -17,17 +29,48 @@
             int first = 0, second = 0, third = 0, fourth = 0;
 
             // code here
+            for (int i = 0;i < n;i++)
+            {
+                bool s1 = double.TryParse(Console.ReadLine(),out double x);
+                bool s2 = double.TryParse(Console.ReadLine(), out double y);
+                if (x > 0 && y >0)
+                {
+                    first++;
+                }
+                else if (x < 0 && y > 0)
+                {
+                    second++;
+                }
+                else if (x < 0 &&  y <  0)
+                {
+                    third++;
+                }
+                else if (x > 0 && y < 0)
+                {
+                    fourth++;
+                }
+            }
 
-            // end
+                // end
 
-            return (first, second, third, fourth);
+                return (first, second, third, fourth);
         }
         public int Task3(int n)
         {
             int count = 0;
 
             // code here
-
+            for (int i = 0; i <n; i++)
+            {
+                bool s1 = int.TryParse(Console.ReadLine(), out int a1);
+                bool s2 = int.TryParse(Console.ReadLine(), out int a2);
+                bool s3 = int.TryParse(Console.ReadLine(), out int a3);
+                bool s4 = int.TryParse(Console.ReadLine(), out int a4);
+                if (a1 > 3 && a2 > 3 && a3 > 3 && a4 > 3) 
+                {
+                    count++;
+                }
+            }
             // end
 
             return count;
@@ -37,6 +80,23 @@
             int serias = 0;
 
             // code here
+            int tasktime = 10;
+            int serialstime;
+            while (time < 1440)
+            {
+                if (tasks >0)
+                {
+                    time += tasktime;
+                    tasktime += 5;
+                    tasks--;
+                }
+                else
+                {
+                    bool s = int.TryParse(Console.ReadLine(), out int seriastime);
+                    time += seriastime;
+                    serias++;
+                }
+            }
 
             // end
 
@@ -46,10 +106,49 @@
         {
 
             // code here
+            switch (number)
+            {
+                case 1:
+                    power += 10;
+                    intellect -= 5; 
+                    break;
+                case 2:
+                    agility += 5;
+                    power -= 5;
+                    intellect -= 5;
+                    break;
+                case 3:
+                    power += 10;
+                    intellect -= 5;
+                    break;
+                case 4:
+                    agility += 15;
+                    power -= 10;
+                    intellect -= 10;
+                    break;
+                case 5:
+                    intellect += 7;
+                    power -= 5;
+                    break;
 
+
+            }
+            if (power < 0)
+            {
+                power = 0;
+            }
+            if (intellect < 0)
+            {
+                intellect = 0;
+            }
+            if (agility < 0)
+            {
+                agility = 0;
+            }
             // end
 
             return (power, agility, intellect);
         }
+        
     }
 }
