45820981e232720aeb8ddf514d32408e37e74b17
ashurov18 <ashurovburhoniddin08@gmail.com>
2025-10-05 20:28:50 +0300

    Update White.cs

--- Lab3/White.cs
+++ Lab3/White.cs
@@ -9,7 +9,13 @@ namespace Lab3
             double averageHeight = 0;
 
             // code here
+            int s = 0;
+            for (int i = 0; i != n; i++)
+            {
+                s += Convert.ToInt16(Console.ReadLine());
+            }
 
+            if (s != 0) averageHeight = (double)s / n;
             // end
 
             return averageHeight;
@@ -19,7 +25,11 @@ namespace Lab3
             double bestResult = 0;
 
             // code here
-
+            for (int i = 0; i != n; i++)
+            {
+                result = Convert.ToDouble(Console.ReadLine());
+                if (result < bestResult) bestResult = result;
+            }
             // end
 
             return bestResult;
@@ -29,7 +39,10 @@ namespace Lab3
             int count = 0;
 
             // code here
-
+            for (int i = 0; i != n; i++)
+            {
+                if (Convert.ToDouble(Console.ReadLine()) <= limit) count++;
+            }
             // end
 
             return count;
@@ -39,7 +52,18 @@ namespace Lab3
             int hours = 0;
 
             // code here
-
+            while (amount < maxAmount)
+            {
+                if (hours % 5 != 4)
+                {
+                    amount++;
+                }
+                else
+                {
+                    amount -= 2;
+                }
+                hours++;
+            }
             // end
 
             return hours;
@@ -49,10 +73,22 @@ namespace Lab3
             double area = 0;
 
             // code here
-
+            switch (type)
+            {
+                case 1:
+                    area = r * r;
+                    break;
+                case 2:
+                    area = Math.PI * r * r;
+                    break;
+                case 3:
+                    area = (r * r * Math.Sqrt(3)) / 4;
+                    break;
+            }
             // end
 
             return area;
         }
     }
+
 }
