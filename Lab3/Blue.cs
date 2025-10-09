e547c52f79a69c5242d0aa27bcd432df1b795853
Миша <moatazhamwi9@gmail.com>
2025-10-09 15:55:40 +0300

    Update Blue.cs

--- Lab3/Blue.cs
+++ Lab3/Blue.cs
@@ -16,14 +16,12 @@ namespace Lab3
 
             return milk;
         }
-
         public (int first, int second, int third, int fourth) Task2(int n)
         {
             int first = 0, second = 0, third = 0, fourth = 0;
 
             // code here
-            double[] coords =
-            {
+            double[] coords = {
                 1.2, 1.7, 0, 0, 4.5, 0, -1, 1.5, -0.5, -0.5,
                 0.2, 0.7, 2, 0, 0.5, 0.5, -1, 1.5, 0.5, 0.1
             };
@@ -39,16 +37,14 @@ namespace Lab3
 
             return (first, second, third, fourth);
         }
-
         public int Task3(int n)
         {
             int count = 0;
 
             // code here
-            int[] marks =
-            {
-                3,4,2,2,5,4,5,5,5,5,5,5,4,4,4,4,4,4,2,5,4,3,3,4,5,2,2,3,4,5,
-                5,4,3,4,4,5,4,5,3,4
+            int[] marks = {
+                3,4,2,2,5,4,5,5,5,5,5,5,4,4,4,4,4,4,2,5,
+                4,3,3,4,5,2,2,3,4,5,5,4,3,4,4,5,4,5,3,4
             };
             for (int i = 0; i < n && i < marks.Length; i++)
                 if (marks[i] == 5) count++;
@@ -56,17 +52,12 @@ namespace Lab3
 
             return count;
         }
-
         public (int tasks, int serias) Task4(int time, int tasks)
         {
             int serias = 0;
 
             // code here
-            int[] mins =
-            {
-                125,120,125,115,117,121,122,120,120,117,
-                126,135,130,132,133
-            };
+            int[] mins = { 125,120,125,115,117,121,122,120,120,117,126,135,130,132,133 };
             int total = 0;
             for (int i = 0; i < mins.Length; i++)
             {
@@ -81,57 +72,32 @@ namespace Lab3
 
             return (tasks, serias);
         }
-
         public (int power, int agility, int intellect) Task5(int power, int agility, int intellect, int number)
         {
 
             // code here
-            if (number == 0)
+            if (number == 1)
             {
-                return (power, agility, intellect);
-            }
-            else if (number == 1)
-            {
-                power += 11;
-                agility += 1;
+                power += 10;
+                intellect -= 5;
+                if (intellect < 0) intellect = 0;
             }
             else if (number == 2)
             {
-                agility += 7;
+                agility += 5;
+                power = power < 5 ? 0 : power / 2;
+                intellect = intellect < 5 ? 0 : intellect / 2;
             }
             else if (number == 3)
             {
-                power += 13;
-                agility += 3;
+                power += 10;
+                intellect -= 5;
+                if (intellect < 0) intellect = 0;
             }
             else if (number == 5)
             {
-                agility += 5;
-                intellect += 12;
-            }
-            else if (number == 10)
-            {
-                power += 20;
-                agility += 10;
-                intellect += 5;
-            }
-            else if (number == 11)
-            {
-                power += 5;
-                agility += 15;
-                intellect += 5;
-            }
-            else if (number == 12)
-            {
-                power += 22;
-                agility += 12;
+                power = 0;
                 intellect += 7;
-            }
-            else if (number == 25)
-            {
-                power += 25;
-                agility += 25;
-                intellect += 25;
             }
             // end
 
