73397f277b818c5271e4acc5f27feaf8983ad2e8
dashausova <m2502203@edu.misis.ru>
2025-10-09 17:15:41 +0300

    УсоваДарья Бивт25-12 синия лига

--- Lab3/Blue.cs
+++ Lab3/Blue.cs
@@ -1,4 +1,4 @@
-﻿namespace Lab3
+namespace Lab3
 {
     public class Blue
     {
@@ -8,6 +8,21 @@
 
             // code here
 
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write($"Введите вес ученика {i} (кг): ");
+                double weight = double.Parse(Console.ReadLine());
+
+                if (weight < norma)
+                {
+                    milk += glass;
+                }
+            }
+
+
+
+            return milk / 1000.0;
             // end
 
             return milk;
@@ -17,6 +32,85 @@
             int first = 0, second = 0, third = 0, fourth = 0;
 
             // code here
+            int s1 = first;
+            int s2 = second;
+            int s3 = third;
+            int s4 = fourth;
+            List<string> k1 = new List<string>();
+            List<string> k2 = new List<string>();
+            List<string> k3 = new List<string>();
+            List<string> k4 = new List<string>();
+            // code here
+            for (int i = 0; i != n; i++)
+            {
+                Console.Write("x = ");
+                double x = Convert.ToDouble(Console.ReadLine());
+                Console.Write("y = ");
+                double y = Convert.ToDouble(Console.ReadLine());
+                if (x > 0 && y > 0)
+                {
+                    s1 += 1;
+                    k1.Add(Convert.ToString(x) + " " + Convert.ToString(y));
+                }
+                if (x < 0 && y < 0)
+                {
+                    s2 += 1;
+                    k2.Add(Convert.ToString(x) + " " + Convert.ToString(y));
+                }
+                if (x < 0 && y > 0)
+                {
+                    s3 += 1;
+                    k3.Add(Convert.ToString(x) + " " + Convert.ToString(y));
+                }
+                if (x > 0 && y < 0)
+                {
+                    s4 += 1;
+                    k4.Add(Convert.ToString(x) + " " + Convert.ToString(y));
+                }
+
+
+            }
+            //Console.WriteLine("1-й = " + k1.Distinct().Count());
+            Console.WriteLine(s1);
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach (var item in k1.Distinct())
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(s3);
+            //Console.WriteLine("2-й = " + k3.Distinct().Count());
+            foreach (var item in k3.Distinct())
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(s2);
+            //Console.WriteLine("3-й = " + k2.Distinct().Count());
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach (var item in k2.Distinct())
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(s4);
+            //Console.WriteLine("4-й = " + k4.Distinct().Count());
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach (var item in k4.Distinct())
+            {
+                Console.WriteLine(item);
+            }
+
+            first = s1;
+            second = s3;
+            third = s2;
+            fourth = s4;
 
             // end
 
@@ -28,6 +122,18 @@
 
             // code here
 
+
+            for (int i = 0; i != n; i++)
+            {
+                bool Flag = true;
+                for (int j = 0; j != 4; j++)
+                {
+                    int s = Convert.ToInt32(Console.ReadLine());
+                    if (s < 4) Flag = false;
+                }
+                if (Flag) count++;
+
+            }
             // end
 
             return count;
@@ -35,18 +141,71 @@
         public (int tasks, int serias) Task4(int time, int tasks)
         {
             int serias = 0;
+            int TaskTime = 10;
 
-            // code here
 
-            // end
 
+
+            while (time / 60 < 24)
+            {
+                if (tasks > 0)
+                {
+                    time += TaskTime;
+                    TaskTime += 5;
+                    tasks--;
+                }
+                else
+                {
+                    time += Convert.ToInt32(Console.ReadLine());
+                    serias++;
+                }
+            }
             return (tasks, serias);
         }
+        // end
+
+
+
+
         public (int power, int agility, int intellect) Task5(int power, int agility, int intellect, int number)
         {
 
             // code here
+            if (number == 1)
+            {
+                power += 10;
+                intellect -= 5;
+                if (intellect < 0) intellect = 0;
+            }
+            else if (number == 2)
+            {
+                agility += 5;
+                power -= 5;
+                intellect -= 5;
+                if (power < 0) power = 0;
+                if (intellect < 0) intellect = 0;
+            }
+            else if (number == 3)
+            {
+                intellect -= 5;
+                power += 10;
+                if (intellect < 0) intellect = 0;
+            }
+            else if (number == 4)
+            {
+                agility += 15;
+                power -= 10;
+                intellect -= 10;
+                if (power < 0) power = 0;
+                if (intellect < 0) intellect = 0;
 
+            }
+            else if (number == 5)
+            {
+                intellect += 7;
+                power -= 5;
+                if (power < 0) power = 0;
+            }
             // end
 
             return (power, agility, intellect);
